7d978e4769ac0b59c0be9c86a8dc23df7495ea0a
¨roberjacobo¨ <robjacobox@gmail.com>
2024-02-22 11:38:42 -0600

    fist section complete / cron implemented

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import { Server } from "./presentation/server";
+
+(() => {
+    Server.start();
+})();
---
+++ src/presentation/server.ts
@@ -0,0 +1,38 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+    public static start() {
+        console.log("Server started...");
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'https://google.com';
+                new CheckService(
+                    () => console.log(`${url} is ok`),
+                    (error) => console.log(error),
+                ).execute(url);
+                // new CheckService().execute('http://localhost:3000');
+            }
+        );
+
+        CronService.createJob(
+            '*/2 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('2 seconds', date);
+            }
+        );
+
+        CronService.createJob(
+            '*/3 * * * * *',
+            () => {
+                const date = new Date();
+                console.log('3 seconds', date);
+            }
+        );
+
+    }
+    // end class
+}
